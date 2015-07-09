00a50941929e90d43704783851fe17565298214a
Judah Lamm <Judah.Lamm@gmail.com>
2015-07-08 14:23:22 -1000

    working solution for clockwise

--- MatrixRotator.js
+++ MatrixRotator.js
@@ -118,25 +118,23 @@ MatrixRotator.prototype.rotateStep = function(direction, layer) {
 var matrix = this.matrix;
 
 var theLength = this.matrix.length;
-var tempArr = [];
-var finalArr = [];
 var radius = Math.ceil(theLength/2);
 
 var magicBig = (theLength - 1) - (radius-layer);
 var magicSmall =  (radius - layer);
+var magicCounter = theLength - magicBig;
 
 var topArr = [];
 var botArr = [];
 var leftArr = [];
 var rightArr = [];
 
-
-
   if(layer < 1 || layer > radius){
       throw new RangeError("there are only 3 layers in this onion");
     }
 
-  if(argument[0] !== 'CW' || argument[0] !== 'CCW' )
+    console.log(arguments[0]);
+  if(arguments[0] !== 'ClockWise' || arguments[0] !== 'CounterClockWise' ){
     //console.log('starting', matrix);
 
       //get me the rows/columns into arrays
@@ -149,13 +147,17 @@ var rightArr = [];
 
     //Clockwise Solution
     if(direction === 'ClockWise'){
-      //puts them back into the matrix
-      for(var j = magicSmall; j <= magicBig; j++){
-        matrix[magicSmall][j] = leftArr.pop();
-        matrix[magicBig][j] = rightArr.pop();
-        matrix[j][magicSmall] = botArr.shift();
-        matrix[j][magicBig] = topArr.shift();
+      leftArr.shift();
+      botArr.shift()
+      var count = magicSmall;
+      for(var i = magicCounter; i <= magicBig; i++){
+        matrix[magicSmall][i] = topArr.shift();
+        matrix[count][magicSmall] = leftArr.shift();
+        matrix[i][magicBig] = rightArr.shift();
+        matrix[magicBig][count] = botArr.shift();
+        count++
       }
+
     }else{
     //Counterclockwise Solution
       //puts them back into the matrix
@@ -169,67 +171,11 @@ var rightArr = [];
       //console.log('ending', matrix);
 
     }
-
-    //work in progress....
-    // got the outsides on a layer 3
-
-    var theLength = m.length;
-var newM = [[],[],[],[],[],[]]
-
-var m = [
-              [1, 2, 3, 4, 5, 6],
-              [7, 8, 9, 10,11,12],
-              [13,14,15,16,17,18],
-              [19,20,21,22,23,24],
-              [25,26,27,28,29,30],
-              [31,32,33,34,35,36]
-            ]
-
-var t = [1, 2, 3, 4, 5, 6];
-var b = [31,32,33,34,35,36];
-var l = [1, 7,13,19,25,31];
-var r = [6,12,18,24,30,36];
-
-//console.log(l.shift());
-
-//Top
-t.pop(); //6
-l.shift(); //1
-var count = 0;
-for(var i = 0; i < theLength; i++){
-    newM[i][count] = l.shift();
-    for(var j = 1; j < theLength; j++){
-        newM[i][j] = t.shift();
-    }
-}
-
-//Bottom
-b.shift()
-countr= 0;
-for(var i = 1; i < theLength; i++){
-    newM[i][5] = r.shift();
-    newM[5][countr] = b.shift();
-    countr++
-}
+  }else{
+    console.log('not a command');
+  }
 
 
-//inners
-
-// for(var i = 0; i <theLength; i++){
-
-// }
-
-console.log('newM',newM);
-
-
-
-
-
-
-
-
-
-
-  return this.matrix;
+return this.matrix;
 
 };
